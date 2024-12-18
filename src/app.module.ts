c919decc227dbf08d83f40229e4733afd4a77a55
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2024-12-18 12:26:09 -0300

    created connection with database and created migration and post and get userEntitties

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_SCHEMA,
+      host: process.env.DB_HOST_API,
+      password: process.env.DB_PASS,
+      username: process.env.DB_USER,
+      port: Number(process.env.DB_PORT),
+      type: 'postgres',
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
+      migrations: [`${__dirname}/migrations/{.ts,*.js}`],
+      migrationsRun: true,
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
