996012d71d14109e5a153b1a034923f11c3d1bfb
= <=>
2025-02-08 18:03:31 -0300

    structure for cart created

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { StateModule } from './state/state.module';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
   imports: [
@@ -38,6 +39,7 @@ import { ProductModule } from './product/product.module';
     JwtModule,
     CategoryModule,
     ProductModule,
+    CartModule,
   ],
   controllers: [],
   providers: [
